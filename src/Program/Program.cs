2b7fcdc5a31f51882a5760150473d00837a66f03
HelperDays <alehernandezprincipal@gmail.com>
2024-09-16 23:53:01 -0300

    Errores arreglados del merge

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,6 +6,10 @@ public class Program
     static void Main(string[] args)
     {
         // Crear ítems de ataque y defensa
+        // Crear ítems de ataque y defensa{
+        var pistolaDePerno1 = new PistolaDePerno("Pistola de Perno de 3 tiros",ataque:5,BalasIniciales:60);
+        var hachaEnano = new HachaEnana(nombre: "Hacha doble pesada", ataque: 40,defensa:0);//enano
+        var armaduraEnano = new ItemDeDefensaEnano(nombre: "Armadura de acero enana", defensa: 30);//enano
         var arma1 = new ItemDeAtaque("Espada", 25);
         var escudo1 = new ItemDeDefensa("Escudo de hierro", 20);
         var armadura1 = new ItemDeDefensa("Armadura de placas", 15);
@@ -15,6 +19,8 @@ public class Program
         var arco1 = new Arco("Flechas de hielo",10,15);
         var capaElfo = new CapaElfo("Capa magica", 10, 30);
         
+        var itemsEnano = new List<ItemEnano> { hachaEnano, armaduraEnano };
+        var enano = new Enano(nombre: "Karaz Ankor", itemsEnano, vida: 250, pistolaDePerno1,hachaEnano);//enano
         var itemsArtoria = new List<Item> { arma1, escudo1, armadura1 };
         var itemsEvil = new List<Item> { arma2, escudo2, armadura2 };
         var itemsElfo = new List<Item> { arco1, escudo2, armadura2, capaElfo };
@@ -32,10 +38,18 @@ public class Program
         hero.GetStats();
         enemigo.GetStats();
         elfo.GetStats();
+        enano.GetStats();
+        
         //Aplicamos metodos para mostrar su funcionamiento
         hero.DealDamage(enemigo);  // Caballero ataca a enemigo
         elfo.DealDamage(enemigo);
         elfo.DealDamage(enemigo); // Elfo ataca con arco 2 veces, la flechas bajar en 2 
+        
+        enano.AlternarArma();//enano alterna el arma entre la pistola de perno o el hacha
+        enano.DealDamage(enemigo);// enano ataca a un enemigo
+        enano.AlternarArma();//cambia de arma para atacar con el hacha/pistola
+        enano.DealDamage(enemigo);//ataca con el
+        
         //Mostramos nuevamente el status actual del enemigo, deberia tener menos vida
         enemigo.GetStats();
         //Por ultimo llamamos al metodo HealDamage y luego mostramos sus stats para ver que realmente se curo
